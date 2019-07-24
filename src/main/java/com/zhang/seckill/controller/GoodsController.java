eb51b116ae14140dfa0eb94956c1ba22d493d0b2
ZHANG <2952184251@qq.com>
2019-07-24 23:40:04 +0800

    增加了登陆功能，实现了分布式session。

--- src/main/java/com/zhang/seckill/controller/GoodsController.java
+++ src/main/java/com/zhang/seckill/controller/GoodsController.java
@@ -4,6 +4,7 @@ package com.zhang.seckill.controller;
 import com.zhang.seckill.domain.SeckillService;
 import com.zhang.seckill.domain.SeckillUser;
 import com.zhang.seckill.redis.RedisService;
+import com.zhang.seckill.redis.SeckillUserKey;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -11,6 +12,8 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.HttpServletResponse;
 
 @Controller
 @RequestMapping("/goods")
@@ -22,13 +25,13 @@ public class GoodsController {
     RedisService redisService;
 
     @RequestMapping("/to_list")
-    public String toList(Model model, @CookieValue(value= SeckillService.COOKIE_NAME_TOKEN,required = false)String cookieToken,
+    public String toList(HttpServletResponse response,Model model, @CookieValue(value= SeckillService.COOKIE_NAME_TOKEN,required = false)String cookieToken,
                          @RequestParam(value=SeckillService.COOKIE_NAME_TOKEN,required = false) String paramToken){
         if(StringUtils.isEmpty(cookieToken)&&StringUtils.isEmpty(paramToken)){
             return "login";
         }
         String token = StringUtils.isEmpty(paramToken) ? cookieToken:paramToken;
-        SeckillUser user = seckillService.getByToken(token);
+        SeckillUser user = seckillService.getByToken(response,token);
         model.addAttribute("user",user);
         return "goods_list";
     }
